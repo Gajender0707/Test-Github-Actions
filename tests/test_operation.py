1b2f886a6a7da0de7e8fd364a93e91b89a49e2e6
Gajender <Iamsanju0707@gmail.com>
2024-11-27 12:10:14 +0530

    udating the workflow

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,13 +1,13 @@
 from src.maths_operations import add,sub
 
 
-def add_test():
+def test_add():
     assert add(3,8)==11, "addition failed"
     assert add(2,3)==5, "addition failed"
 
 
 
-def sub_test():
+def test_sub():
     assert sub(5,2)==3, "substraction failed"
     assert sub(-1,-8)==-9, "substraction failed"
 
